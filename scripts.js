9903d3a39421fb6622970f8157d546f05a891a85
promy <promyaaa@gmail.com>
2024-08-03 11:11:08 +0600

    secret key generated

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const generateKeyBtn = document.getElementById('generate');
-    const secretKeyDisplay = document.getElementById('secret-key');
+    const generateBtn = document.getElementById('generate');
+    const secretKey = document.getElementById('secret-key');
     const pinInput = document.getElementById('pinToMatch');
     const message = document.getElementById('message');
     const keys = document.querySelectorAll('.key');
 
-    
+    let secret = '';
+
+    function generateSecretKey() {
+        secret = Math.floor(100000 + Math.random() * 900000).toString();
+        secretKey.textContent = secret;
+    }
+
+    generateBtn.addEventListener('click', generateSecretKey);
 
 })
