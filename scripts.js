5b367c921b9e064da21a68a9d2245969c14bc86d
promy <promyaaa@gmail.com>
2024-08-03 11:05:02 +0600

    cache html elements

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const generateKeyBtn = document.getElementById('generate');
+    const secretKeyDisplay = document.getElementById('secret-key');
+    const pinInput = document.getElementById('pinToMatch');
+    const message = document.getElementById('message');
+    const keys = document.querySelectorAll('.key');
+
+    
+
+})
