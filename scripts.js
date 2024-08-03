e382e8d66f483ff6a2fb1cd750ee09d5cb270765
promy <promyaaa@gmail.com>
2024-08-03 11:44:52 +0600

    pin compare, notification css added and input limit to 6 digit

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const generateBtn = document.getElementById('generate');
     const secretKey = document.getElementById('secret-key');
     const pinInput = document.getElementById('pinToMatch');
-    const message = document.getElementById('message');
+    const notification = document.getElementById('notification');
     const keys = document.querySelectorAll('.key');
 
     let secret = '';
@@ -16,16 +16,38 @@ document.addEventListener('DOMContentLoaded', function() {
         pinInput.value = '';
     }
 
+    function comparePin() {
+        console.log(secret)
+        console.log(pinInput.value)
+
+        if (pinInput.value === secret) {
+            notification.textContent = 'SUCCESS! YOU ARE AWESOME!!';
+            notification.style.color = '#6cc788';
+        } else {
+            notification.textContent = 'SORRY! TRY AGAIN!!';
+            notification.style.color = '#f44455';
+        }
+    }
+
     generateBtn.addEventListener('click', generateSecretKey);
+
+    pinInput.addEventListener('input', function() {
+        // Ensure the value is a string and limit to 6 digits
+        pinInput.value = pinInput.value.replace(/\D/g, '').substring(0, 6);
+    });
 
     keys.forEach(key => {
         key.addEventListener('click', function() {
             if (this.id === 'clear') {
+                // on click clear button
                 clearInput();
             } else if (this.id === 'submit') {
-                // submit
+                //on click submit button 
+                comparePin();
             } else {
                 // show number
+                const newValue = pinInput.value + this.getAttribute('data-value');
+                pinInput.value = newValue.substring(0, 6);
             }
         });
     });
